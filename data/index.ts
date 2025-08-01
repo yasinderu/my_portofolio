76ada3107f95601acb6d2c93ea3515339d30b568
yasinderu <m.yasin449@gmail.com>
2025-08-01 13:32:07 +0700

    add projects

--- data/index.ts
+++ data/index.ts
@@ -86,22 +86,29 @@ export const projects = [
     iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/exjs.png", "/node.png"],
     link: "https://www.pret.co.uk/en-GB/club-pret",
   },
-  // {
-  //   id: 3,
-  //   title: "AI Image SaaS - Canva Application",
-  //   des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
-  //   img: "/p3.svg",
-  //   iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
-  //   link: "https://github.com/adrianhajdin/ai_saas_app",
-  // },
-  // {
-  //   id: 4,
-  //   title: "Animated Apple Iphone 3D Website",
-  //   des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
-  //   img: "/p4.svg",
-  //   iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
-  //   link: "https://github.com/adrianhajdin/iphone",
-  // },
+  {
+    id: 3,
+    title: "A Netflix like streaming Web App",
+    des: "A website application with the UI and UX similar to netflix.",
+    img: "/p3.webp",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/mongo.svg"],
+    link: "https://yasin-streaming.vercel.app/",
+  },
+  {
+    id: 4,
+    title: "AI Inventory Management System",
+    des: "Inventory Management System to track items within the inventory using AI Chat.",
+    img: "/",
+    iconLists: [
+      "/next.svg",
+      "/tail.svg",
+      "/ts.svg",
+      "/python.svg",
+      "/fastapi.svg",
+      "/posgre.svg",
+    ],
+    link: "https://github.com/yasinderu/ai-supplychain",
+  },
 ];
 
 export const companies = [
